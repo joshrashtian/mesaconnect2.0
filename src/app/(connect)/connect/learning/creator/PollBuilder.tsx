733e8d1c96c756f5e2e79b5c03092941fe993281
Joshua Rashtian <joshrashtian1@gmail.com>
2024-03-03 23:35:15 -0800

    Visual Work on Poll builder

--- src/app/(connect)/connect/learning/creator/PollBuilder.tsx
+++ src/app/(connect)/connect/learning/creator/PollBuilder.tsx
@@ -4,7 +4,8 @@ import React, { useRef, useState } from "react";
 const PollBuilder = () => {
   const [poll, setPoll] = useState();
   const [options, setOptions] = useState<any[]>([]);
-  const [context, setContext] = useState();
+  const [context, setContext] = useState<File>();
+  const [correct, setCorrect] = useState<number | undefined>();
 
   const contextRef: any = useRef();
 
@@ -19,9 +20,23 @@ const PollBuilder = () => {
       {context ? (
         <>
           <h2 className="font-mono my-4">Context:</h2>
-          <ul className="p-2 bg-slate-200 rounded-full w-1/3 flex justify-center">
-            <h1 className="text-slate-700 font-mono">{context?.name}</h1>
-          </ul>
+          <section>
+            {context.type.includes("image/") && (
+              <img src={URL.createObjectURL(context)} className=" w-28 " />
+            )}
+            <ul className="p-2 px-5 bg-slate-200 rounded-full w-1/3 flex justify-between items-center">
+              <p />
+              <h1 className="text-slate-700 font-mono">{context?.name}</h1>
+              <button
+                onClick={() => {
+                  setContext(undefined);
+                }}
+                className="font-mono text-red-600"
+              >
+                x
+              </button>
+            </ul>
+          </section>
         </>
       ) : (
         <button
@@ -39,8 +54,10 @@ const PollBuilder = () => {
       <input
         type="file"
         onChange={(e) => {
-          setContext(e.target.files[0]);
+          if (!e.target.files) return;
+          setContext(e.target?.files[0]);
         }}
+        accept=".jpg, .png, .jpeg, .gif, .pdf"
         ref={contextRef}
         hidden
       />
@@ -49,12 +66,23 @@ const PollBuilder = () => {
           options.map((e, i) => {
             return (
               <ul
-                className="w-[49%] h-32 p-4 flex flex-col justify-center items-center shadow-sm bg-slate-200 rounded-xl"
+                className={`${
+                  i === correct ? "bg-green-200" : "bg-zinc-200"
+                } w-[49%] duration-500 h-32 p-4 flex flex-row justify-center items-center shadow-sm  rounded-xl`}
                 key={i}
               >
+                <h1
+                  onClick={() => {
+                    correct != i ? setCorrect(i) : setCorrect(undefined);
+                  }}
+                  className={`text-2xl ${
+                    i === correct ? "text-green-800" : "text-teal-600"
+                  } `}
+                >
+                  {i + 1}
+                </h1>
                 <input
                   type="text"
-                  className=" bg-transparent "
                   onChange={(e) => {
                     setOptions((options) =>
                       options?.map((d, j) => {
@@ -66,7 +94,7 @@ const PollBuilder = () => {
                       })
                     );
                   }}
-                  className="p-2 px-5 w-full rounded-full"
+                  className="p-2 px-5 w-full text-2xl text-zinc-600 rounded-full outline-none bg-transparent"
                   placeholder="Option"
                   contentEditable
                 />
@@ -79,7 +107,12 @@ const PollBuilder = () => {
           }}
           className="w-[49%] h-32 p-4 flex justify-center items-center border-2 rounded-xl border-slate-500 border-dashed "
         >
-          <h1>Create New Option</h1>
+          <h1 className="font-mono">Create New Option</h1>
+        </button>
+      </section>
+      <section className="w-full flex justify-center">
+        <button onClick={() => {}}>
+          <h1>Submit</h1>
         </button>
       </section>
     </main>
