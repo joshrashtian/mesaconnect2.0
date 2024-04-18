412adc748fbb7a24fee640be39628d0031234f11
joshrashtian <joshrashtian1@gmail.com>
2024-04-18 13:12:14 -0700

    header for polls / search

--- src/_components/navigation.tsx
+++ src/_components/navigation.tsx
@@ -1,81 +1,86 @@
-"use client";
-import React, { useContext, useEffect, useState } from "react";
-import { AnimatePresence, motion } from "framer-motion";
-import Link from "next/link";
-import { supabase } from "../../config/mesa-config";
-import { userContext } from "@/app/AuthContext";
+'use client'
+import React, { useContext, useEffect, useState } from 'react'
+import { AnimatePresence, motion } from 'framer-motion'
+import Link from 'next/link'
+import { supabase } from '../../config/mesa-config'
+import { userContext } from '@/app/AuthContext'
+import { IoHome } from 'react-icons/io5'
 
 const Dock = () => {
-  const [selected, setSelected] = useState("");
-  const [profURL, setProfURL] = useState<string | undefined>();
-  const [isHovered, setIsHovered] = useState(false);
-  const [profID, setProfID] = useState<string | undefined>();
+  const [selected, setSelected] = useState('')
+  const [profURL, setProfURL] = useState<string | undefined>()
+  const [isHovered, setIsHovered] = useState(false)
+  const [profID, setProfID] = useState<string | undefined>()
 
-  const userData = useContext<any>(userContext);
+  const userData = useContext<any>(userContext)
 
-  const SamplePhoto = require("../../src/_assets/photos/UserIcon.png");
+  const SamplePhoto = require('../../src/_assets/photos/UserIcon.png')
 
   useEffect(() => {
     const fetchURL = async () => {
-      const user = await supabase.auth.getUser();
+      const user = await supabase.auth.getUser()
 
       const { data, error } = await supabase
-        .from("profiles")
+        .from('profiles')
         .select()
-        .eq("id", user.data.user?.id)
-        .single();
+        .eq('id', user.data.user?.id)
+        .single()
 
       if (error) {
-        console.log(error);
-        setProfURL(undefined);
-        return;
+        console.log(error)
+        setProfURL(undefined)
+        return
       }
 
-      setProfID(data.id);
-      setProfURL(data.avatar_url);
-    };
+      setProfID(data.id)
+      setProfURL(data.avatar_url)
+    }
 
-    fetchURL();
-  }, []);
+    fetchURL()
+  }, [])
 
   const tabs = [
     {
-      name: "Home",
-      link: "/",
+      name: 'Home',
+      link: '/'
     },
     {
-      name: "Profile",
-      link: `/profile/${profID}`,
+      name: 'Profile',
+      link: `/profile/${profID}`
     },
     {
-      name: "Social",
-      link: "/social",
+      name: 'Social',
+      link: '/social'
     },
     {
-      name: "Learning",
-      link: "/learning",
+      name: 'Learning',
+      link: '/learning'
     },
     {
-      name: "Studio",
-      link: "/builder",
+      name: 'Studio',
+      link: '/builder'
     },
     {
-      name: "Admin",
-      link: "/admin",
-      permissions: ["admin"],
+      name: 'Settings',
+      link: '/settings'
     },
-  ];
+    {
+      name: 'Admin',
+      link: '/admin',
+      permissions: ['admin']
+    }
+  ]
 
   return (
     <div className="w-full bottom-8 left-0 h-16 fixed justify-center items-center z-50 flex">
       <section
         onMouseEnter={() => {
-          setIsHovered(true);
+          setIsHovered(true)
         }}
         onMouseLeave={() => {
-          setIsHovered(false);
+          setIsHovered(false)
         }}
-        className="group peer bg-white origin-center shadow-md rounded-full h-full w-16 hover:2xl:w-[40%] hover:w-[70%] max-w-2xl justify-center items-center duration-500 hover:scale-110 ease-in-out  "
+        className="group peer bg-white origin-center shadow-md rounded-full h-full w-16 hover:2xl:w-[60%] hover:w-[70%] max-w-3xl justify-center items-center duration-500 2xl:duration-700 hover:scale-110 ease-in-out  "
       >
         <AnimatePresence>
           {profURL && !isHovered && (
@@ -101,15 +106,12 @@ const Dock = () => {
               animate={{ opacity: 1 }}
               exit={{ opacity: 0 }}
               transition={{ delay: 0.6, duration: 0.2 }}
-              className="w-full flex-row delay-500 flex justify-center gap-2 items-center duration-200"
+              className="w-full h-full flex-row delay-500 flex justify-center gap-2  items-center duration-200"
             >
               {tabs.map((tab, index) => {
                 if (tab.permissions) {
-                  if (
-                    !tab.permissions.includes(userData.userData?.role) ||
-                    !userData
-                  ) {
-                    return null;
+                  if (!tab.permissions.includes(userData.userData?.role) || !userData) {
+                    return null
                   }
                 }
 
@@ -130,7 +132,7 @@ const Dock = () => {
                       </h1>
                     </Link>
                   </motion.li>
-                );
+                )
               })}
             </motion.ul>
           </AnimatePresence>
@@ -141,7 +143,7 @@ const Dock = () => {
         <h1 className="font-bold">{selected}</h1>
       </section>
     </div>
-  );
-};
+  )
+}
 
-export default Dock;
+export default Dock
--- src/app/(connect)/connect/learning/(sub-pages)/polls/page.tsx
+++ src/app/(connect)/connect/learning/(sub-pages)/polls/page.tsx
@@ -1,23 +1,26 @@
-"use client";
-import React from "react";
-import { usePollModal } from "../../LearningContext";
-import FeaturedPolls from "../../_components/FeaturedPolls";
-import ClassPollList from "../../_components/ClassSpecific";
+'use client'
+import React from 'react'
+import { usePollModal } from '../../LearningContext'
+import FeaturedPolls from '../../_components/FeaturedPolls'
+import ClassPollList from '../../_components/ClassSpecific'
 
 const PollsHome = () => {
-  const modal = usePollModal();
+  const modal = usePollModal()
 
   return (
-    <main className="flex flex-col gap-12">
-      <h1 className="inline-block bg-clip-text text-transparent bg-gradient-to-br from-blue-500 to-teal-500 font-bold font-eudoxus text-5xl ">
-        Questions & Polls
-      </h1>
-      <section className="gap-10 flex flex-col">
+    <main className="flex flex-col absolute min-h-screen w-screen pt-32 left-0 top-0 gap-12">
+      <header className="h-28 flex fixed items-center px-10 w-full bg-gradient-to-b from-zinc-200 to-transparent top-0 left-0">
+        <h1 className="inline-block bg-clip-text text-transparent bg-gradient-to-br from-orange-500 to-teal-600 font-bold font-eudoxus text-5xl ">
+          MESApolls
+        </h1>
+      </header>
+
+      <section className="gap-10 flex flex-col p-10">
         <FeaturedPolls />
-        <ClassPollList title="For Calculus I" classlist={} />
+        <ClassPollList title="For Calculus I" />
       </section>
     </main>
-  );
-};
+  )
+}
 
-export default PollsHome;
+export default PollsHome
