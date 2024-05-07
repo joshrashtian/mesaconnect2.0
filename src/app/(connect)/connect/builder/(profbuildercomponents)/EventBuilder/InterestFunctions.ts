8d7710bc5495b93238d5f527ad7a924ac50073fa
joshrashtian <joshrashtian1@gmail.com>
2024-05-07 13:21:05 -0700

    Interest Function

--- src/app/(connect)/connect/builder/(profbuildercomponents)/EventBuilder/InterestFunctions.ts
+++ src/app/(connect)/connect/builder/(profbuildercomponents)/EventBuilder/InterestFunctions.ts
@@ -4,11 +4,12 @@ import { Interest } from "../../../(tabs)/social/community/InterestButtons";
 import { serverside } from "../../../../../../../config/serverside";
 
 export async function submitInterests (interest: Interest) {
-    const { data, error } = await serverside.from('interests').insert({
-            interest: interest.interest,
-            fieldType: interest.fieldType,
-        
-    }).select()
+    let { data, error } = await serverside
+  .rpc('addinterest', {
+    newfieldtype: interest.interest, 
+    newinterest: interest.fieldType, 
+    newinterestid: interest.id ? interest.id : null 
+  })
 
     if (error) {
         console.error(error);
