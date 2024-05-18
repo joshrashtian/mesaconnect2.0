0a0f3f4078020a2bb08f4b09a377dc6eab6dca31
Joshua Rashtian <joshrashtian1@gmail.com>
2024-05-18 13:28:29 -0700

    Sitemap Fix

--- src/app/news/sitemap.ts
+++ src/app/news/sitemap.ts
@@ -7,12 +7,17 @@ export default async function sitemap({
     id: number
 }): Promise<MetadataRoute.Sitemap> {
 
+
+
     const { data, error } = await serverside.from('newsposts').select("*").lt("id", 100)
 
-    //@ts-ignore
-    return data?.map((product) => ({
-        url: `https://mesaconnect.io/news?arid=${id}`,
-        lastModified: product.date,
-    }))
+    const newsArticles: MetadataRoute.Sitemap = data ? data?.map((article) => ({
+        url: `https://mesaconnect.io/news?arid=${article.id}`,
+        lastModified: new Date(article.created_at)
+    })) : [{ url: 'http://mesaconnect.io/news'}]
+
+    return [
+            ...newsArticles
+    ]
 
 }
