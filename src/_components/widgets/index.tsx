9516ba877b613304ac19a51381efd399220cb0e1
joshrashtian <joshrashtian1@gmail.com>
2024-09-12 15:33:26 -0700

    Recent Posts Widget

--- src/_components/widgets/index.tsx
+++ src/_components/widgets/index.tsx
@@ -1,20 +1,45 @@
-import CurrentEventSegment from '../socialhub/CurrentEventSegment'
+import CurrentEventSegment from "../socialhub/CurrentEventSegment";
+import { RecentPostsHome } from "../socialhub/RecentPostsHome.1";
 
 export const widgets = [
   {
-    name: 'Your Events',
-    size: 'tall',
-    style: 'bg-zinc-200',
+    name: "Your Events",
+    size: "tall",
+    style: "bg-zinc-200",
     widget: () => <CurrentEventSegment />,
     showcase: () => (
       <section
-        className={`w-52 h-52 flex flex-col gap-2 p-5 justify-center items-center dark:bg-zinc-600 rounded-xl bg-zinc-100 `}
+        className={`flex h-32 w-32 flex-col items-center justify-center gap-2 rounded-xl bg-zinc-300 p-5 dark:bg-zinc-600`}
       >
-        <h1 className="font-light text-left text-md dark:text-white">Your Events</h1>
-        <ul className="w-full h-full bg-white rounded-xl dark:bg-zinc-800/40" />
-        <ul className="w-full h-full bg-white rounded-xl dark:bg-zinc-800/40" />
-        <ul className="w-full h-full bg-white rounded-xl dark:bg-zinc-800/40" />
+        <h1 className="text-md text-left font-light dark:text-white">
+          Your Events
+        </h1>
+        <ul className="h-full w-full rounded-xl bg-white dark:bg-zinc-800/40" />
+        <ul className="h-full w-full rounded-xl bg-white dark:bg-zinc-800/40" />
+        <ul className="h-full w-full rounded-xl bg-white dark:bg-zinc-800/40" />
       </section>
-    )
-  }
-]
+    ),
+  },
+  {
+    name: "Recent Posts",
+    size: "tall",
+    style: "bg-zinc-200",
+    widget: () => (
+      <ul className="pb-24">
+        <RecentPostsHome />
+      </ul>
+    ),
+    showcase: () => (
+      <section
+        className={`flex h-32 w-32 flex-col items-center justify-center gap-2 rounded-xl bg-white p-5 dark:bg-zinc-600`}
+      >
+        <h1 className="text-md text-left font-light dark:text-white">
+          Recent Posts
+        </h1>
+        <ul className="h-full w-full rounded-xl bg-gradient-to-tr from-purple-600 to-red-500 dark:bg-zinc-800/40" />
+        <ul className="h-full w-full rounded-xl bg-zinc-500 dark:bg-zinc-800/40" />
+        <ul className="h-full w-full rounded-xl bg-zinc-500 dark:bg-zinc-800/40" />
+      </section>
+    ),
+  },
+];
