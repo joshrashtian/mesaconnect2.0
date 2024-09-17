4845a02ada152ee3e14aee6f6f8f58c75128f4d7
joshrashtian <joshrashtian1@gmail.com>
2024-09-17 16:14:43 -0700

    documentation page (wip)

---
+++ src/_components/docs/docheader.tsx
@@ -0,0 +1,20 @@
+"use client";
+import React from "react";
+import { motion } from "framer-motion";
+const DocsHeader = () => {
+  return (
+    <header className="fixed left-0 top-0 flex h-16 w-screen flex-row items-center bg-zinc-800 p-3">
+      <motion.h2 className="font-eudoxus text-sm text-slate-300 shadow-black lg:text-xl">
+        MESA
+        <motion.span className="text text-white drop-shadow-xl">
+          Docs
+        </motion.span>
+        <motion.span className="mx-2 rounded-full bg-red-400 px-2 text-xs text-black dark:bg-black dark:text-white">
+          beta
+        </motion.span>
+      </motion.h2>
+    </header>
+  );
+};
+
+export default DocsHeader;
