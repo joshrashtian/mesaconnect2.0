b292482f9444a59b806b2d17161ab367df0f3395
joshrashtian <joshrashtian1@gmail.com>
2024-10-07 15:39:41 -0700

    Improved OpenGraph for Profiles

--- src/app/(connect)/connect/(profiles)/profile/[id]/layout.tsx
+++ src/app/(connect)/connect/(profiles)/profile/[id]/layout.tsx
@@ -5,15 +5,38 @@ import { serverside } from "../../../../../../../config/serverside";
 export async function generateMetadata({ params }: { params: { id: string } }) {
   const { data, error } = await serverside
     .from("profiles")
-    .select("real_name")
+    .select("real_name, bio, avatar_url, major")
     .eq("id", params.id);
 
+  const description = () => {
+    if (!data) {
+      return "Error Fetching Bio";
+    }
+    if (data[0].bio && data[0].major) {
+      return `${data[0].major} - ${data[0].bio}`;
+    }
+    if (data[0].bio) {
+      return data[0].bio;
+    }
+    if (data[0].major) {
+      return data[0].major;
+    } else return "No Bio Set.";
+  };
   return {
     title: data
       ? `${data[0].real_name}`
       : error
         ? "Error Fetching User"
         : "Fetching Name",
+    opengraph: {
+      title: data
+        ? `${data[0].real_name}`
+        : error
+          ? "Error Fetching User"
+          : "Fetching Name",
+      description,
+      images: data ? (data[0].avatar_url ? data[0].avatar_url : "") : "",
+    },
   };
 }
 
