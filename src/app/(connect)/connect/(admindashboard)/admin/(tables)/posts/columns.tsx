181bc367fe01d373cd3b60a6cb9e74a70143d96c
joshrashtian <joshrashtian1@gmail.com>
2024-11-20 12:57:51 -0800

    Admin Dashboard Reform ft. shadcn

--- src/app/(connect)/connect/(admindashboard)/admin/(tables)/posts/columns.tsx
+++ src/app/(connect)/connect/(admindashboard)/admin/(tables)/posts/columns.tsx
@@ -1,15 +1,16 @@
 "use client";
 
-import { PostType } from "@/_assets/types";
+import { PostType, UserData } from "@/_assets/types";
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
 import {
   DropdownMenu,
-  DropdownMenuTrigger,
   DropdownMenuContent,
-  DropdownMenuLabel,
   DropdownMenuItem,
+  DropdownMenuLabel,
   DropdownMenuSeparator,
-} from "@radix-ui/react-dropdown-menu";
-import { Button } from "@react-email/components";
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { type ColumnDef } from "@tanstack/react-table";
 import { MoreHorizontal } from "lucide-react";
 
@@ -23,7 +24,7 @@ export const columns: ColumnDef<PostType>[] = [
     accessorKey: "id",
   },
   {
-    header: "Creator",
+    header: "Creator ID",
     accessorKey: "userid",
   },
   {
@@ -37,26 +38,96 @@ export const columns: ColumnDef<PostType>[] = [
   {
     id: "actions",
     cell: ({ row }) => {
-      const payment = row.original;
+      const user = row.original;
 
       return (
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
             <Button className="h-8 w-8 p-0">
               <span className="sr-only">Open menu</span>
-              <MoreHorizontal className="h-4 w-4 cursor-pointer" />
+              <MoreHorizontal className="h-4 w-4 cursor-pointer rounded-3xl p-0.5 hover:bg-zinc-200" />
             </Button>
           </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
+          <DropdownMenuContent className="h-fit w-48">
             <DropdownMenuLabel>Actions</DropdownMenuLabel>
             <DropdownMenuItem
-              onClick={() => navigator.clipboard.writeText(payment.id)}
+              onClick={() => navigator.clipboard.writeText(user.id)}
             >
               Copy payment ID
             </DropdownMenuItem>
             <DropdownMenuSeparator />
             <DropdownMenuItem>View customer</DropdownMenuItem>
             <DropdownMenuItem>View payment details</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+];
+
+export const UserColums: ColumnDef<UserData>[] = [
+  {
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={
+          table.getIsAllPageRowsSelected() ||
+          (table.getIsSomePageRowsSelected() && "indeterminate")
+        }
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
+  {
+    header: "User ID",
+    accessorKey: "id",
+  },
+  {
+    header: "Name",
+    accessorKey: "real_name",
+  },
+  {
+    header: "Major",
+    accessorKey: "major",
+  },
+  {
+    header: "Campus",
+    accessorKey: "college",
+  },
+  {
+    header: "Role",
+    accessorKey: "role",
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const user = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="cursor-pointer rounded-3xl hover:bg-zinc-200" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="h-fit w-48">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem
+              onClick={() => navigator.clipboard.writeText(user.id)}
+            >
+              Copy payment ID
+            </DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
       );
