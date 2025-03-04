e23bec28e7a81ad06ae94cbcd72ba4b0672b1e04
joshrashtian <joshrashtian1@gmail.com>
2025-03-03 22:58:22 -0800

    feat: Add user authentication check for file uploads in room menu

--- src/app/room/[id]/(components)/Menu.tsx
+++ src/app/room/[id]/(components)/Menu.tsx
@@ -15,6 +15,7 @@ import {
   IoInformationOutline,
   IoMenuOutline,
   IoCardOutline,
+  IoLockClosedOutline,
 } from "react-icons/io5";
 import { useRoomContext } from "../../RoomContext";
 import { useRef, useState } from "react";
@@ -24,6 +25,7 @@ import { DM_Serif_Text, Golos_Text, Koh_Santepheap } from "next/font/google";
 import RoomSettings from "./RoomSettings";
 import Files from "./Files";
 import RoomMenu from "./RoomMenu";
+import { FileUp } from "lucide-react";
 
 const font = Golos_Text({
   subsets: ["latin"],
@@ -150,92 +152,101 @@ const Menu = () => {
               <IoArrowUp className="text-zinc-100 duration-300 group-hover:text-zinc-300" />
             </button>
           </form>
-          <div className="grid grid-cols-2 gap-2">
-            <button
-              onClick={() => imageUploadRef.current?.click()}
-              className="flex h-full flex-col items-center justify-center rounded-md bg-zinc-300 p-2"
-            >
-              <IoImageOutline />
-              IMAGE UPLOAD
-            </button>
-            <button
-              onClick={() => PDFUploadRef.current?.click()}
-              className="flex h-full flex-col items-center justify-center rounded-md bg-zinc-300 p-2"
-            >
-              <IoFileTrayStackedOutline />
-              PDF UPLOAD
-            </button>
-            <input
-              type="file"
-              accept="image/*"
-              className="hidden"
-              ref={imageUploadRef}
-              onChange={async (e) => {
-                const file = e.target.files?.[0];
-                if (!file) return;
-                const { data: FileData, error } = await supabase.storage
-                  .from(`rooms/${data.id}`)
-                  .upload(file.name, file);
-                if (error) {
-                  alert("Error uploading image: " + error.message);
-                } else {
-                  console.log(FileData);
-                  let url = FileData.path;
-                  await supabase.channel(data.id).send({
-                    type: "broadcast",
-                    event: "message",
-                    payload: {
-                      type: "image",
-                      image: `https://gnmpzioggytlqzekuyuo.supabase.co/storage/v1/object/public/rooms/${data.id}/${file.name}`,
-                      user_id: user?.user?.id,
-                      user:
-                        user?.user?.user_metadata.real_name ??
-                        user?.user?.user_metadata.full_name ??
-                        user?.user?.user_metadata.name ??
-                        "Guest",
-                      room_id: data.id,
-                      created_at: new Date().toISOString(),
-                    },
-                  });
-                }
-              }}
-            />
-            <input
-              type="file"
-              accept="application/pdf"
-              className="hidden"
-              ref={PDFUploadRef}
-              onChange={async (e) => {
-                const file = e.target.files?.[0];
-                if (!file) return;
-                const { data: FileData, error } = await supabase.storage
-                  .from(`rooms/${data.id}`)
-                  .upload(file.name, file);
-                if (error) {
-                  alert("Error uploading image: " + error.message);
-                } else {
-                  console.log(FileData);
-                  let url = FileData.path;
-                  await supabase.channel(data.id).send({
-                    type: "broadcast",
-                    event: "message",
-                    payload: {
-                      type: "PDF",
-                      pdf: `https://gnmpzioggytlqzekuyuo.supabase.co/storage/v1/object/public/rooms/${data.id}/${file.name}`,
-                      user_id: user?.user?.id,
-                      user:
-                        user?.user?.user_metadata.real_name ??
-                        user?.user?.user_metadata.full_name ??
-                        user?.user?.user_metadata.name ??
-                        "Guest",
-                      room_id: data.id,
-                      created_at: new Date().toISOString(),
-                    },
-                  });
-                }
-              }}
-            />
-          </div>
+
+          {user.user ? (
+            <div className="grid h-32 grid-cols-2 gap-2 text-xl">
+              <button
+                onClick={() => imageUploadRef.current?.click()}
+                className="flex h-full flex-col items-center justify-center gap-2 rounded-md bg-gradient-to-tr from-red-600 to-orange-600 p-2 text-white duration-300 hover:opacity-80"
+              >
+                <IoImageOutline className="text-4xl" />
+                Image Upload
+              </button>
+              <button
+                onClick={() => PDFUploadRef.current?.click()}
+                className="flex h-full flex-col items-center justify-center gap-2 rounded-md bg-gradient-to-tr from-blue-500 to-blue-800 p-2 text-white duration-300 hover:opacity-80"
+              >
+                <FileUp className="text-4xl" />
+                PDF Upload
+              </button>
+              <input
+                type="file"
+                accept="image/*"
+                className="hidden"
+                ref={imageUploadRef}
+                onChange={async (e) => {
+                  const file = e.target.files?.[0];
+                  if (!file || !user.user) return;
+                  const { data: FileData, error } = await supabase.storage
+                    .from(`rooms/${data.id}`)
+                    .upload(file.name, file);
+                  if (error) {
+                    alert("Error uploading image: " + error.message);
+                  } else {
+                    console.log(FileData);
+                    let url = FileData.path;
+                    await supabase.channel(data.id).send({
+                      type: "broadcast",
+                      event: "message",
+                      payload: {
+                        type: "image",
+                        image: `https://gnmpzioggytlqzekuyuo.supabase.co/storage/v1/object/public/rooms/${data.id}/${file.name}`,
+                        user_id: user?.user?.id,
+                        user:
+                          user?.user?.user_metadata.real_name ??
+                          user?.user?.user_metadata.full_name ??
+                          user?.user?.user_metadata.name ??
+                          "Guest",
+                        room_id: data.id,
+                        created_at: new Date().toISOString(),
+                      },
+                    });
+                  }
+                }}
+              />
+              <input
+                type="file"
+                accept="application/pdf"
+                className="hidden"
+                ref={PDFUploadRef}
+                onChange={async (e) => {
+                  const file = e.target.files?.[0];
+                  if (!file || !user.user) return;
+                  const { data: FileData, error } = await supabase.storage
+                    .from(`rooms/${data.id}`)
+                    .upload(file.name, file);
+                  if (error) {
+                    alert("Error uploading image: " + error.message);
+                  } else {
+                    console.log(FileData);
+                    let url = FileData.path;
+                    await supabase.channel(data.id).send({
+                      type: "broadcast",
+                      event: "message",
+                      payload: {
+                        type: "PDF",
+                        pdf: `https://gnmpzioggytlqzekuyuo.supabase.co/storage/v1/object/public/rooms/${data.id}/${file.name}`,
+                        user_id: user?.user?.id,
+                        user:
+                          user?.user?.user_metadata.real_name ??
+                          user?.user?.user_metadata.full_name ??
+                          user?.user?.user_metadata.name ??
+                          "Guest",
+                        room_id: data.id,
+                        created_at: new Date().toISOString(),
+                      },
+                    });
+                  }
+                }}
+              />
+            </div>
+          ) : (
+            <ol className="flex h-full w-full flex-col items-center justify-center gap-2 rounded-md p-2 text-center text-2xl">
+              <IoLockClosedOutline className="text-4xl" />
+              Uploading documents and images is only available for signed-in
+              users.
+            </ol>
+          )}
         </>
       )}
       {category === "settings" && <RoomSettings />}
