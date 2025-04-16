62a8a52cf7474f815a98b78a08baa227db261768
joshrashtian <joshrashtian1@gmail.com>
2025-04-15 17:20:04 -0700

    refactor: Optimize cookie handling in serverside.ts by using a dedicated cookieStore instance

--- config/serverside.ts
+++ config/serverside.ts
@@ -1,6 +1,7 @@
 import { createServerClient, type CookieOptions } from "@supabase/ssr";
 import { cookies } from "next/headers";
 
+const cookieStore = cookies();
 
 export const serverside = createServerClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
@@ -9,13 +10,13 @@ export const serverside = createServerClient(
     
     cookies: {
       get(name: string) {
-        return cookies().get(name)?.value;
+        return cookieStore.get(name)?.value;
       },
       set(name: string, value: string, options: CookieOptions) {
-            cookies().set({ name, value, ...options });
+            cookieStore.set({ name, value, ...options });
         },
         remove(name: string, options: CookieOptions) {
-            cookies().set({ name, value: "", ...options });
+            cookieStore.set({ name, value: "", ...options });
         },
 
     },
