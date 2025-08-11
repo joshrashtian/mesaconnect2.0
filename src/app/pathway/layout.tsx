cc399b7629dc4f8694dcf14e8e195ba8b42eafc8
joshrashtian <joshrashtian1@gmail.com>
2025-08-10 20:09:43 -0700

    feat: Wrap children in Suspense for loading state in PathwayLayout component

--- src/app/pathway/layout.tsx
+++ src/app/pathway/layout.tsx
@@ -1,10 +1,12 @@
 "use server";
-import React from "react";
+import React, { Suspense } from "react";
 import HomePageHeader from "../news/(homepage)/header";
 
 const PathwayLayout = async ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex flex-col gap-4 p-14 font-eudoxus">{children}</div>
+    <div className="flex flex-col gap-4 p-14 font-eudoxus">
+      <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>
+    </div>
   );
 };
 
